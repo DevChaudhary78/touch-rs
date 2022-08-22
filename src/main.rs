9ca2e2efef0fb2ac924e88d893fceb9087aca226
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2022-08-22 11:30:36 +0530

    base funcionality working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_name = &args[1];
+
+    let mut file = File::create(file_name).expect("Error encountered while creating a file");
+
+    print!("{}", file_name);
 }
