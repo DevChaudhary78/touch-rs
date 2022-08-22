cdcafa39ce577d4de48947966500b24530362ac1
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2022-08-22 11:31:35 +0530

    removed unused code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let file_name = &args[1];
 
-    let mut file = File::create(file_name).expect("Error encountered while creating a file");
+    File::create(file_name).expect("Error encountered while creating a file");
 
     print!("{}", file_name);
 }
