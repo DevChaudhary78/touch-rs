5bac0dd7cb346f1df213f22bbe04338ce3a941c0
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2022-08-23 09:36:43 +0530

    added a feature to create multiple files in one command

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::fs::File;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let file_name = &args[1];
+    for i in 1..args.len() {
+        let file_name = &args[i];
 
-    File::create(file_name).expect("Error encountered while creating a file");
-
-    print!("{}", file_name);
+        File::create(file_name).expect("Error encountered while creating a file.");
+    }
 }
