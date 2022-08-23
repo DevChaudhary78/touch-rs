c5f2d2533dc205fc8abe0fa4c2230dd0b162cf3e
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2022-08-23 19:10:40 +0530

    restructured the code using lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
 use std::env;
-use std::fs::File;
+
+mod lib;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    for i in 1..args.len() {
-        let file_name = &args[i];
-
-        File::create(file_name).expect("Error encountered while creating a file.");
-    }
+    let mut args: Vec<String> = env::args().collect();
+    lib::Cli::start(&mut args);
 }
